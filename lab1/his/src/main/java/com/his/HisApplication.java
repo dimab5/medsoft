7429dcd8ce8d4bd50bfe0faa90a9eff9b9c026c7
Дмитрий <dimanb555@gmail.com>
2025-10-20 02:45:44 +0300

    add doctor api module

--- lab1/his/src/main/java/com/his/HisApplication.java
+++ lab1/his/src/main/java/com/his/HisApplication.java
@@ -1,6 +1,5 @@
 package com.his;
 
-import com.his.services.HL7ParserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
